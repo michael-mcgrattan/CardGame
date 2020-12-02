813ef97bfec665c161df5faf54a5b4973b93904b
Michael McGrattan <mcgrattanmichael@gmail.com>
2020-12-02 15:09:27 +0000

    MM, MH - we have wrote the test for the remove 3 random queens function

--- tests/Test_ScabbyQueen.py
+++ tests/Test_ScabbyQueen.py
@@ -1,10 +1,15 @@
-import unittest
+import unittest, ScabbyQueen
 
+minimumQueen = 1
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class ScabbyQueenTest(unittest.TestCase):
+    def test_removeQueens(self):
+        deck = ScabbyQueen.removeQueens()
+        noOfQueen = sum('Q' in s for s in deck)
+        self.assertEqual(noOfQueen, minimumQueen)
 
+def main():
+    unittest.main()
 
 if __name__ == '__main__':
     unittest.main()
