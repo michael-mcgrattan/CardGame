62c763a5d9bca7cd18334b61d50a640bcbab1aa4
Michael McGrattan <mcgrattanmichael@gmail.com>
2020-12-02 15:09:53 +0000

    MM, MH - we have set up the deck by removing 3 random queens

--- ScabbyQueen.py
+++ ScabbyQueen.py
@@ -1,6 +1,15 @@
-import playingCard
+import playingCard as pc
 
+minimumQueen = 1
 
-def createDeck():
-    deck=playingCard.generateDeck()
-    return deck
+def removeQueens():
+    count = 0
+    deck = pc.generateDeck()
+    shuffledDeck = pc.shuffleCards(deck)
+    for card in shuffledDeck:
+        if card[-1] == "Q":
+            shuffledDeck.remove(card)
+            count += 1
+        if count == (4 - minimumQueen):
+            break
+    return shuffledDeck
